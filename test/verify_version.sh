6d2da95199072c58863e6f0489df847ed89fb9e9
Antti Kultanen <antti.kultanen@molukki.com>
2025-10-01 16:28:32 +0300

    Update tests

--- test/verify_version.sh
+++ test/verify_version.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "Verify compiled version string $(./nflog_dns --version) matches build version ${VERSION}"
-if [ -z "${VERSION}" ]
+echo "Verify compiled version string $(./nflog_dns --version) matches build version ${1}"
+if [ -z "${1}" ]
 then
+	echo missing version...
 	VERSION="$(cut -d '"' -f 2 version.h)"
+else
+	VERSION="${1}"
 fi
 
 ./nflog_dns --version | grep -F "version ${VERSION}"

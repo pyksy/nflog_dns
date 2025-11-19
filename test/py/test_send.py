aba639c2c4757c051073f0f293d6858826955c42
Antti Kultanen <antti.kultanen@molukki.com>
2025-11-19 15:57:37 +0200

    Update tests

--- test/py/test_send.py
+++ test/py/test_send.py
@@ -6,8 +6,8 @@
 import socket
 import sys
 
-if len(sys.argv) != 3:
-    print(f"Usage: {sys.argv[0]} <DEST_IP> <DEST_PORT>")
+if len(sys.argv) != 4:
+    print(f"Usage: {sys.argv[0]} <DEST_IP> <DEST_PORT> <PACKET_TYPE>")
     sys.exit(1)
 
 DEST_IP = sys.argv[1]
@@ -16,12 +16,39 @@ try:
 except ValueError:
     print("Error: DEST_PORT must be an integer.")
     sys.exit(1)
+PACKET_TYPE = sys.argv[3].upper()
 
-# DNS reply packet: example.com resolves to 127.0.0.1
-packet = bytes.fromhex(
-    '123481800001000100000000076578616d706c6503636f6d0000010001'
-    'c00c000100010000003c00047f000001'
-)
+if PACKET_TYPE == 'A':
+    # example.com A 127.0.0.1
+    packet = bytes.fromhex(
+        '123481800001000100000000076578616d706c6503636f6d0000010001'
+        'c00c000100010000003c00047f000001'
+    )
+elif PACKET_TYPE == 'AAAA':
+    # example.com AAAA to ::1
+    packet = bytes.fromhex(
+        '123481800001000100000000076578616d706c6503636f6d00001c0001'
+        'c00c001c00010000003c001000000000000000000000000000000001'
+    )
+elif PACKET_TYPE == 'CNAME':
+    # www.example.com CNAME _
+    packet = bytes.fromhex(
+        '12348180000100010000000003777777076578616d706c6503636f6d0000050001'
+        'c00c000500010000003c0002c010'
+    )
+elif PACKET_TYPE == 'PTR':
+    # 1.0.0.127.in-addr.arpa PTR example.com
+#    packet = bytes.fromhex(
+#        '1234818000010001000000000131013001300331323707696e2d61646472046172706100000c0001'
+#        'c00c000c00010000003c0011076578616d706c6503636f6d00'
+#    )
+    packet = bytes.fromhex(
+        '1234818000010001000000000131013001300331323707696e2d61646472046172706100'
+        '000c0001c00c000c00010000003c000d076578616d706c6503636f6d00'
+    )
+else:
+    print(f"Error: PACKET_TYPE must be either 'A', 'AAAA', 'CNAME' or 'PTR', got '{PACKET_TYPE}'")
+    sys.exit(1)
 
 # Create UDP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
--- test/run_tests.sh
+++ test/run_tests.sh
@@ -8,10 +8,46 @@ if [[ "${EUID}" -ne 0 ]]; then
 	exit 1
 fi
 
-echo "=== Start ==="
-
 IP="172.31.53.123"
 DIR="$(dirname $(realpath "${0}"))"
+declare -a PACKET_TYPES=(
+	a
+	aaaa
+	cname
+	ptr
+)
+
+fail_count=0
+
+send_packets() {
+	for TYPE in "${PACKET_TYPES[@]}"
+	do
+		echo -n "Start UDP receiver ... "
+		exec 3< <(python3 "${DIR}/py/test_recv.py" "${IP}")
+		LISTENPID="${!}"
+		read LISTENPORT <&3
+		echo "PID ${LISTENPID} UDP port ${LISTENPORT}"
+
+		echo -n "Send ${TYPE^^} reply packet to listener ... "
+		python3 "${DIR}/py/test_send.py" "${IP}" "${LISTENPORT}" $TYPE
+		echo "done"
+	done
+}
+
+verify_packets() {
+	for TYPE in "${PACKET_TYPES[@]}"
+	do
+		echo -n "Verify ${TYPE^^} reply was logged ... "
+		LOGSTRING="$(grep "${IP} reply ${TYPE^^} .*example\.com" "${NFLOGTEMP}")"
+		if [ -n "${LOGSTRING}" ]
+		then
+			echo "SUCCESS"
+		else
+			echo "FAIL"
+			((fail_count++))
+		fi
+	done
+}
 
 echo -n "Setup dummy interface nflog0 ... "
 ip link add nflog0 type dummy
@@ -19,41 +55,55 @@ ip addr add "${IP}"/32 dev nflog0
 ip link set up dev nflog0
 echo "done"
 
-echo -n "Start UDP receiver ... "
-exec 3< <(python3 "${DIR}/py/test_recv.py" "${IP}")
-LISTENPID="${!}"
-read LISTENPORT <&3
-echo "PID ${LISTENPID} UDP port ${LISTENPORT}"
-
 echo -n "Setup iptables NFLOG target ... "
 iptables -N nflog_dns_logger
 iptables -A nflog_dns_logger -j NFLOG --nflog-group 123
-iptables -I INPUT 1 -t filter -p udp -d "${IP}" --sport 53 --dport "${LISTENPORT}" -j nflog_dns_logger
+iptables -I INPUT 1 -t filter -p udp -d "${IP}" --sport 53 -j nflog_dns_logger
 echo  "done"
 
-echo -n "Start nflog_dns logging ... "
+echo -n "Start nflog_dns ... "
 NFLOGTEMP="$(mktemp "/tmp/nflog_XXXXXXXX.temp")"
 "${DIR}/../nflog_dns" >"${NFLOGTEMP}" &
 NFLOGPID="${!}"
 echo "PID ${NFLOGPID}"
 
-echo -n "Send DNS reply packet to listener ... "
-python3 "${DIR}/py/test_send.py" "${IP}" "${LISTENPORT}"
-echo "done"
-
+send_packets
 sleep 2
-
-echo "nflog_dns logfile content:"
 cat "${NFLOGTEMP}"
-
-echo -n "Verify DNS reply was logged ... "
-LOGSTRING="$(grep "example\.com.*127\.0\.0\.1" "${NFLOGTEMP}")"
-[ -n "${LOGSTRING}" ] && echo "SUCCESS" || echo "FAIL"
 
 echo -n "Stop nflog_dns ... "
 kill -HUP "${NFLOGPID}"
-rm -f "${NFLOGTEMP}"
 echo "done"
+sleep 1
+
+verify_packets
+rm -f "${NFLOGTEMP}"
+
+((fail_count > 0)) && exit 1 || echo
+
+ARGS=""
+NFLOGTEMP="$(mktemp "/tmp/nflog_XXXXXXXX.temp")"
+for TYPE in "${PACKET_TYPES[@]}"
+do
+	ARGS="--${TYPE}=no ${ARGS}"
+done
+echo -n "Start nflog_dns ${ARGS} ... "
+"${DIR}/../nflog_dns" $ARGS >"${NFLOGTEMP}" &
+NFLOGPID="${!}"
+echo "PID ${NFLOGPID}"
+
+send_packets
+sleep 2
+cat "${NFLOGTEMP}"
+
+echo -n "Stop nflog_dns ... "
+kill -HUP "${NFLOGPID}"
+echo "done"
+sleep 1
+
+echo "NOTE: Following tests are expected to fail:"
+verify_packets
+rm -f "${NFLOGTEMP}"
 
 echo -n "Tear down iptables  ... "
 iptables -D INPUT 1 -t filter
@@ -67,6 +117,5 @@ ip addr del "${IP}"/32 dev nflog0
 ip link del nflog0
 echo "done"
 
-echo "=== END ==="
+((fail_count == ${#PACKET_TYPES[@]})) || exit 1
 
-[ -n "${LOGSTRING}" ] && exit 0 || exit 1
